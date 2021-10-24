f7b32a6a818e4447a6bb38a3b5af16c2609cfd56
Aline Souza <ds.alinesouza@gmail.com>
2021-10-23 22:21:36 -0300

    exercise bignumber

---
+++ bigNumber/bigNumber.js
@@ -0,0 +1,8 @@
+let firstNumber = window.prompt("Enter a number:")
+let secondNumber = window.prompt("Enter other number:")
+if (firstNumber > secondNumber) {
+    window.alert(`The ${firstNumber} it's bigger`)
+} else {
+    window.alert(`The ${secondNumber} 
+    it's smaller`)
+}
